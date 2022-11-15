e89b960cf3fb688ae999c6e1d2bec1fbfdacd393
mubin-bonu <mubin.me@mail.ru>
2022-11-15 22:06:18 +0600

    initial

---
+++ js/dz4.js
@@ -0,0 +1,9 @@
+function password() {
+    alert('name, age, profession, email');
+}
+
+let months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь', 'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь'];
+let date = new Date();
+let month = date.getMonth();
+alert (months[month]);
+
